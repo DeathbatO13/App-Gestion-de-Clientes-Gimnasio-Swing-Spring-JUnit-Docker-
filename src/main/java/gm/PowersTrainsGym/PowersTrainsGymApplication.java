3858430da3bfea8314faab39cf6a5e0e51fb9094
DeathbatO13 <danielalejo99@gmail.com>
2025-12-29 17:23:24 -0500

    First Config

---
+++ src/main/java/gm/PowersTrainsGym/PowersTrainsGymApplication.java
@@ -0,0 +1,13 @@
+package gm.PowersTrainsGym;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PowersTrainsGymApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PowersTrainsGymApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gm/PowersTrainsGym/PowersTrainsGymApplicationTests.java
@@ -0,0 +1,13 @@
+package gm.PowersTrainsGym;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PowersTrainsGymApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
