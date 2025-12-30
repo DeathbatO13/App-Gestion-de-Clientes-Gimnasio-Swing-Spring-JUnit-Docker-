a8bb42943cd785923d409745a4c1ded9820c5266
DeathbatO13 <danielalejo99@gmail.com>
2025-12-30 13:11:49 -0500

    Repository

---
+++ src/main/java/gm/PowersTrainsGym/repositorio/ClienteRepositorio.java
@@ -0,0 +1,8 @@
+package gm.PowersTrainsGym.repositorio;
+
+import gm.PowersTrainsGym.modelo.Cliente;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ClienteRepositorio extends JpaRepository<Cliente, Integer>{
+
+}
