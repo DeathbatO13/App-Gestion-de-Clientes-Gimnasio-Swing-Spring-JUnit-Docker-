334d564a0b6921ef192fefc0d215bf2fc04df99c
DeathbatO13 <danielalejo99@gmail.com>
2026-01-08 17:09:15 -0500

    Limpiar Campos

--- src/main/java/gm/PowersTrainsGym/gui/PowerTrainsForm.java
+++ src/main/java/gm/PowersTrainsGym/gui/PowerTrainsForm.java
@@ -8,10 +8,7 @@ import org.springframework.stereotype.Component;
 
 import javax.swing.*;
 import javax.swing.table.DefaultTableModel;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
+import java.awt.event.*;
 
 @Component
 public class PowerTrainsForm extends JFrame{
@@ -47,6 +44,7 @@ public class PowerTrainsForm extends JFrame{
         });
 
         eliminarButton.addActionListener(e -> eliminarCliente());
+        limpiarButton.addActionListener(e -> limpiarFormulario());
     }
 
     private void iniciarForma(){
